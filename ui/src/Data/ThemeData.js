c49b9644b7ae05b27bf9d5bd1e884b815f29d564
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2023-06-06 19:44:03 +0300

    #

--- ui/src/Data/ThemeData.js
+++ ui/src/Data/ThemeData.js
@@ -1,4 +1,3 @@
-
 const lightBgThemeColors = ["#D65A31", "#0D7377", "#5432D3", "#00A8CC", "darkgray", "#F037A5", "#916BBF", "#1EAE98"];
 const darkBgThemeColors = ["lightblue", "#E6DDC4", "lightpink", "aquamarine", "lightgray", "lightgreen", "cyan", "#D9D7F1"];
 const extraColors=['purple'];
