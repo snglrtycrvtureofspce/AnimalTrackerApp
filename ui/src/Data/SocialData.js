5f5581ab937d8681b8114e229bc326233a621c9e
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2023-06-13 18:04:30 +0300

    #

--- ui/src/Data/SocialData.js
+++ ui/src/Data/SocialData.js
@@ -1,6 +1,6 @@
 const githubLink="https://github.com/snglrtycrvtureofspce";
 const instaLink="https://instagram.com/";
-const linkedInLink="https://www.linkedin.com/";
+const linkedInLink="https://www.linkedin.com/snglrtycrvtureofspce";
 const twitterLink="https://twitter.com/";
 const emailLink="mailto:asp1rantzenevich@gmail.com";
 
